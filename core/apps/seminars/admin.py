7afae1185870c861da645c797648efc990142cbd
Kirill Shuvalov <112349988+kirshuvl@users.noreply.github.com>
2024-06-02 18:45:11 +0400

    Seminar Application (#4)

---
+++ core/apps/seminars/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+
+from core.apps.seminars.models import Seminar
+
+
+@admin.register(Seminar)
+class SeminarAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "date",
+        "created_at",
+        "updated_at",
+    )
+    list_display_links = (
+        "id",
+        "date",
+    )
+    search_fields = (
+        "id",
+        "date",
+        "created_at",
+        "updated_at",
+    )
+    list_filter = ("date",)
+    ordering = ("id",)
