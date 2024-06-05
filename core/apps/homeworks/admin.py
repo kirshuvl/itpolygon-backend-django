befb614abe9cb15cdc1b421e795460537280e282
Kirill <kirshuvl@gmail.com>
2024-06-05 19:36:56 +0400

    Create HomeworkStepConnectionModel

--- core/apps/homeworks/admin.py
+++ core/apps/homeworks/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from core.apps.homeworks.models import Homework
+from core.apps.homeworks.models import Homework, HomeworkStepConnection
 
 
 @admin.register(Homework)
@@ -27,5 +27,36 @@ class HomeworkAdmin(admin.ModelAdmin):
     list_filter = (
         "author",
         "seminar",
+    )
+    ordering = ("id",)
+
+
+@admin.register(HomeworkStepConnection)
+class HomeworkStepConnectionAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "homework",
+        "step",
+        "is_published",
+        "created_at",
+        "updated_at",
+    )
+    list_display_links = (
+        "id",
+        "homework",
+        "step",
+    )
+    search_fields = (
+        "id",
+        "homework",
+        "step",
+        "is_published",
+        "created_at",
+        "updated_at",
+    )
+    list_filter = (
+        "homework",
+        "step",
+        "is_published",
     )
     ordering = ("id",)
