939c15d067ce1211b01186bde2035c6937f2a4b7
Kirill Shuvalov <112349988+kirshuvl@users.noreply.github.com>
2024-06-16 17:32:06 +0400

    Fix Models (#15)

--- core/apps/courses/lms/serializers.py
+++ core/apps/courses/lms/serializers.py
@@ -65,13 +65,63 @@ class LessonRetrieveSerializer(ModelSerializer):
         )
 
     def get_steps(self, lesson):
-        queryset = [lesson.step for lesson in lesson.lesson_step_connections.all()]
+        queryset = [connection.step for connection in lesson.lesson_step_connections.all()]
 
         return StepRetrieveSerializer(queryset, many=True).data
 
 
+class LessonStatisticsRetrieveSerializer(ModelSerializer):
+    userStatistics = SerializerMethodField()
+
+    class Meta:
+        model = Lesson
+        fields = (
+            "id",
+            "title",
+            "number",
+            "userStatistics",
+        )
+
+    def get_userStatistics(self, lesson):
+
+        total_theoretical_steps = 0
+        completed_theoretical_steps = 0
+        total_practical_steps = 0
+        completed_tpractical_steps = 0
+
+        steps = [connection.step for connection in lesson.lesson_step_connections.all()]
+
+        for step in steps:
+            step_type = step.get_type()
+            user_enroll = step.user_step_enrolls.first()
+            if step_type == "textstep" or step_type == "videostep":
+                total_theoretical_steps += 1
+                if user_enroll and user_enroll.status == "OK":
+                    completed_theoretical_steps += 1
+            else:
+                total_practical_steps += 1
+                if user_enroll and user_enroll.status == "OK":
+                    completed_tpractical_steps += 1
+        total_steps = total_theoretical_steps + total_practical_steps
+        completed_steps = completed_theoretical_steps + completed_tpractical_steps
+
+        return {
+            "totalSteps": total_steps,
+            "completedSteps": completed_steps,
+            "theoreticalSteps": {
+                "total": total_theoretical_steps,
+                "completed": completed_theoretical_steps,
+            },
+            "practicalSteps": {
+                "total": total_practical_steps,
+                "completed": completed_tpractical_steps,
+            },
+        }
+
+
 class TopicRetrieveSerializer(ModelSerializer):
-    lessons = LessonRetrieveSerializer(many=True)
+    lessons = LessonStatisticsRetrieveSerializer(many=True)
+    userStatistics = SerializerMethodField()
 
     class Meta:
         model = Topic
@@ -79,8 +129,49 @@ class TopicRetrieveSerializer(ModelSerializer):
             "id",
             "title",
             "number",
+            "userStatistics",
             "lessons",
         )
+
+    def get_userStatistics(self, topic):
+
+        total_theoretical_steps = 0
+        completed_theoretical_steps = 0
+        total_practical_steps = 0
+        completed_tpractical_steps = 0
+
+        steps = [
+            connection.step
+            for lesson in topic.lessons.all()
+            for connection in lesson.lesson_step_connections.all()
+        ]
+
+        for step in steps:
+            step_type = step.get_type()
+            user_enroll = step.user_step_enrolls.first()
+            if step_type == "textstep" or step_type == "videostep":
+                total_theoretical_steps += 1
+                if user_enroll and user_enroll.status == "OK":
+                    completed_theoretical_steps += 1
+            else:
+                total_practical_steps += 1
+                if user_enroll and user_enroll.status == "OK":
+                    completed_tpractical_steps += 1
+        total_steps = total_theoretical_steps + total_practical_steps
+        completed_steps = completed_theoretical_steps + completed_tpractical_steps
+
+        return {
+            "totalSteps": total_steps,
+            "completedSteps": completed_steps,
+            "theoreticalSteps": {
+                "total": total_theoretical_steps,
+                "completed": completed_theoretical_steps,
+            },
+            "practicalSteps": {
+                "total": total_practical_steps,
+                "completed": completed_tpractical_steps,
+            },
+        }
 
 
 class CourseRetrieveSerializer(ModelSerializer):
