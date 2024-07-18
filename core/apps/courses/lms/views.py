ad5219f5796bb0603ce7e5e0297015790a17e383
Kirill <kirshuvl@gmail.com>
2024-07-19 02:15:22 +0400

    Small Fix

--- core/apps/courses/lms/views.py
+++ core/apps/courses/lms/views.py
@@ -14,6 +14,7 @@ from core.apps.steps.models import (
 from django.db.models import Prefetch
 
 from core.apps.courses.lms.serializers import (
+    CourseListSerializer,
     CourseRetrieveSerializer,
     LessonRetrieveSerializer,
 )
